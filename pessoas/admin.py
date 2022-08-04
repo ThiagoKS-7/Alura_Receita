7b339fe077359d232bdb393162ac5e150f663c9f
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-04 03:56:20 -0300

    configurando admin do app pessoas

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Pessoa
+
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ("id", "nome_pessoa", "idade", "email","telefone")
+    list_display_links = ("id", "nome_pessoa")
+    search_fields = ("nome_pessoa",)
+    list_per_page = 10
+
+admin.site.register(Pessoa,ListandoPessoas)
+
 
 # Register your models here.
