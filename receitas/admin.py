8b033f210575ffa1d6a941885e1c0a12ebba48f5
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-04 03:25:30 -0300

    implementando filtro por categoria e barra de pesquisa no django admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -5,6 +5,9 @@ from .models import Receita
 class ListandoReceitas(admin.ModelAdmin):
     list_display = ("id", "nome_receita", "categoria", "tempo_preparo")
     list_display_links = ("id", "nome_receita")
+    search_fields = ("nome_receita",)
+    list_filter = ("categoria",)
+    list_per_page = 10
 
 
 # Register your models here.
