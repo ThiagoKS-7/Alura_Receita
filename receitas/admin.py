3227f5c76b96af5aad47ab6cb7bf6a6ab31b7869
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-04 03:13:35 -0300

    configurando django admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Receita
 
+
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ("id", "nome_receita", "categoria", "tempo_preparo")
+    list_display_links = ("id", "nome_receita")
+
+
 # Register your models here.
-admin.site.register(Receita)
+admin.site.register(Receita, ListandoReceitas)
