f40d09c09e13c400831a7e612498372f110fa347
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-11 01:31:08 -0300

    4-release: implementando imagens e banners nas receitas e filtros de busca

--- receitas/views.py
+++ receitas/views.py
@@ -4,6 +4,7 @@ from .models import Receita
 # Create your views here.
 def index(request):
     receitas = Receita.objects.order_by("data_receita").filter(publicada=True)
+    print(request)
     data = {
         'receitas': receitas,
     }
@@ -15,3 +16,15 @@ def receitas(request, receita_id):
         'receita': receita
     }
     return render(request,'receitas.html', data);
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by("data_receita").filter(publicada=True)
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            #procura todo nome que contenha o que for escrito no campo de busca
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+    data = {
+        'receitas': lista_receitas
+    }
+    return render(request,'buscar.html',data);
